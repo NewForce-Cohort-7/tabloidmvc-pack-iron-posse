5a63116c75299ee9ae5a9251aac6eb0678570f78
Kiersten White <kierstenswhite@gmail.com>
2023-07-03 13:42:19 -0400

    Added some comments and cleaned up a few things

--- TabloidMVC/Models/ViewModels/PostEditViewModel.cs
+++ TabloidMVC/Models/ViewModels/PostEditViewModel.cs
@@ -1,9 +1,10 @@
-﻿namespace TabloidMVC.Models.ViewModels
+﻿//PostEditViewModel provides the elements needed for the Edit Post page so users can see information from the Posts and a list of the categories, not just their Ids.
+
+namespace TabloidMVC.Models.ViewModels
 {
     public class PostEditViewModel
     {
         public Post Post { get; set; }
         public List<Category> CategoryOptions { get; set; }
-        public int currentUserId { get; set; }
     }
 }
